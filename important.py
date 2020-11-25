2f47b8a462f382197fb8506988eec49d2d4b930c
Jay <68179569+Jay-Surana@users.noreply.github.com>
2020-11-25 10:55:15 +0530

    added  googleJoin
    remember me from your comments - Jay Surana this should work you  can test it too 🙂

--- important.py
+++ important.py
@@ -2,12 +2,20 @@
 import keyboard
 
 def zoomJoin():
-    time.sleep(3)
+    time.sleep(5)
     keyboard.send("tab", do_press=True, do_release=True)
     keyboard.send("tab", do_press=True, do_release=True)
     keyboard.send("enter", do_press=True, do_release=True)
 
 def googleJoin():
-    print("Under construction")
-
-
+    time.sleep(6) # takes time to load cam and glitches sometimes so i set it higher
+    keyboard.send("tab", do_press=True, do_release=True)
+    keyboard.send("tab", do_press=True, do_release=True)
+    keyboard.send("tab", do_press=True, do_release=True)
+    keyboard.send("enter", do_press=True, do_release=True) #turning off mic
+    keyboard.send("tab", do_press=True, do_release=True)
+    keyboard.send("enter", do_press=True, do_release=True) #turning off cam
+    keyboard.send("tab", do_press=True, do_release=True)
+    keyboard.send("tab", do_press=True, do_release=True)
+    keyboard.send("enter", do_press=True, do_release=True) #join
+ 
